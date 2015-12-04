79d398deaf333d2f60df4a73bbd958ec1ee60739
Sara Lands <saralands@Saras-MacBook-Pro.local>
2015-12-04 16:57:53 -0500

    jq

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// $(document).on('ready', function (){
+// 	var foo =$('p.is-big');
+// 	console.log(foo);
+
+
+// 	var callback = function(){
+// 		// alert('Hello World');
+// 	}
+
+
+// 	var someRunner = function(callback){
+// 		console.log('I have run. I am am some runner.');
+// 		callback();
+
+// 	}
+
+// 	someRunner(callback);
+
+
+	
+
+// 	var runBuster = function(){
+// 			$('.buster').animate({
+// 			opacity: .25,
+// 			width: '10%'
+// 		}, 	10000);
+
+// 	};
+
+// 	$('.buster').on('nycda', function (){
+// 		runBuster();
+// 	});
+
+// 	$('jerry').on('customEventRandom', function(){
+// 		alert('heyyyy');
+// 	})
+
+
+
+$("#button").on("click", function(){   alert('you clicked me!') });
+	
+
+
+
+
+
+
+	//$('.lucille').show(4000,runBuster);
+
+
+}); //end doc.ready
+
+
+
+
+// var bar = function(){
+// 	console.log('I am bar');
+// }
+
+// $(document).on('ready', bar);
